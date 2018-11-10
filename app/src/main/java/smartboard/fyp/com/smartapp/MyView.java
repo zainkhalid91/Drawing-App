acf7fdf1e4a2fb151cbb4d4506c3c3d1fa793a0e
zain_ <zainkhalid1991@gmail.com>
2018-11-10 11:52:39 +0500

    New changes

---
+++ app/src/main/java/smartboard/fyp/com/smartapp/MyView.java
@@ -0,0 +1,136 @@
+package smartboard.fyp.com.smartapp;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PointF;
+import android.util.AttributeSet;
+import android.util.SparseArray;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class MyView extends View {
+
+    static MainActivity drawingActivity;
+    private ArrayList<xy> drawList;
+    private float offsetX, offsetY;
+    private boolean touching = false;
+    private Bitmap bm;
+    private SparseArray<PointF> mActivePointers;
+
+    public MyView(Context context) {
+        super(context);
+        init();
+    }
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MyView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public static void getInstance(MainActivity drawingAct) {
+        drawingActivity = drawingAct;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(
+                MeasureSpec.getSize(widthMeasureSpec),
+                MeasureSpec.getSize(heightMeasureSpec));
+    }
+
+    private void init() {
+        bm = BitmapFactory.decodeResource(
+                getResources(),
+                R.drawable.ic_action_name);
+        offsetX = bm.getWidth() / 2;
+        offsetY = bm.getHeight() / 2;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.GRAY);
+        if (touching) {
+            for (xy pt : drawList) {
+                canvas.drawBitmap(bm,
+                        pt.getX() - offsetX,
+                        pt.getY() - offsetY,
+                        null);
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        // get pointer index from the event object
+        int pointerIndex = event.getActionIndex();
+
+        // get pointer ID
+        int pointerId = event.getPointerId(pointerIndex);
+
+        // get masked (not specific to a pointer) action
+        int maskedAction = event.getActionMasked();
+
+        switch (maskedAction) {
+
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_POINTER_DOWN: {
+                // We have a new pointer. Lets add it to the list of pointers
+
+                PointF f = new PointF();
+                f.x = event.getX(pointerIndex);
+                f.y = event.getY(pointerIndex);
+                mActivePointers.put(pointerId, f);
+                break;
+            }
+            case MotionEvent.ACTION_MOVE: { // a pointer was moved
+                for (int size = event.getPointerCount(), i = 0; i < size; i++) {
+                    PointF point = mActivePointers.get(event.getPointerId(i));
+                    if (point != null) {
+                        point.x = event.getX(i);
+                        point.y = event.getY(i);
+                    }
+                }
+                break;
+            }
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_POINTER_UP:
+            case MotionEvent.ACTION_CANCEL: {
+                mActivePointers.remove(pointerId);
+                break;
+            }
+        }
+        invalidate();
+
+        return true;
+    }
+
+    class xy {
+        float x;
+        float y;
+
+        public xy(float x, float y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public float getX() {
+            return x;
+        }
+
+        public float getY() {
+            return y;
+        }
+    }
+
+}
---
+++ app/src/main/java/smartboard/fyp/com/smartapp/WebViewActivity.java
@@ -0,0 +1,99 @@
+package smartboard.fyp.com.smartapp;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+
+
+public class WebViewActivity extends AppCompatActivity {
+    WebView webView;
+    ImageView close;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.web_view_activity);
+        webView = findViewById(R.id.webView);
+       /* close = (ImageView) findViewById(R.id.close_iv);
+        close.setClickable(true);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                close.setImageResource(R.drawable.ic_close);
+                WebViewActivity.this.finish();
+            }
+        });
+*/
+        startWebView("https://www.google.com.pk/");
+
+    }
+
+
+    private void startWebView(String url) {
+        webView.setWebViewClient(new WebViewClient() {
+            ProgressDialog progressDialog;
+
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            public void onLoadResource(WebView view, String url) {
+                if (progressDialog == null) {
+                    // in standard case YourActivity.this
+                    progressDialog = new ProgressDialog(WebViewActivity.this);
+                    progressDialog.setMessage("Loading...");
+                    progressDialog.show();
+                }
+            }
+
+            public void onPageFinished(WebView view, String url) {
+                try {
+                    if (progressDialog.isShowing()) {
+                        progressDialog.dismiss();
+                        progressDialog = null;
+                    }
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            }
+        });
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    public void onBackPressed() {
+        if (webView.copyBackForwardList().getCurrentIndex() > 0) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
